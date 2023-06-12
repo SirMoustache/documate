c0b6109486d85d13127a9888d5ef2aa918672fa3
ATK3Shape <Alexandr.Tkachenko@3shape.com>
2023-06-12 11:39:30 +0300

    Add header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Header() {
+  return (
+    <div className="px-5 py-5 flex justify-between items-center bg-white border-b-2">
+      <div className="font-semibold text-2xl">DocuMate</div>
+    </div>
+  )
+}
