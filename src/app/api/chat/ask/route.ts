3c6019237ae1ecde4c8095a396542fe23d88d9e4
Vitalii Hopaniuk <SirMoustache@users.noreply.github.com>
2023-06-12 08:38:25 +0300

    Add chat ask API endpoint file

---
+++ src/app/api/chat/ask/route.ts
@@ -0,0 +1,5 @@
+import { NextResponse, NextRequest } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  return NextResponse.json({ error: 'Not implemented yet' }, { status: 500 });
+}
