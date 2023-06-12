1549cfa89aaa3e46ba8455e2ef7da59820bb7764
Vitalii Hopaniuk <SirMoustache@users.noreply.github.com>
2023-06-12 20:27:07 +0300

    Implement `trainVectorStore` and `resetVectorStore` utils

--- src/features/VectorStore/VectorStoreInstance.ts
+++ src/features/VectorStore/VectorStoreInstance.ts
@@ -1,15 +1,59 @@
-import { VectorStore } from './types';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
 
-import { makeVectorStoreClient } from './VectorStoreClientFactory';
+import { Document, DocumentSchema } from '@docu/shared/DocumentLoader';
+
+import { VectorStore } from './types';
 import { makeVectorStore } from './VectorStoreFactory';
+import { getVectorStoreClient } from './VectorStoreClientInstance';
+import {
+  OPEN_AI_API_KEY,
+  PINECONE_INDEX_NAME,
+  PINECONE_NAME_SPACE,
+  PINECONE_TEXT_KEY,
+} from './config';
 
 let vectorStore: VectorStore;
 
 export const getVectorStore = async (): Promise<VectorStore> => {
   if (!vectorStore) {
-    const vectorStoreClient = await makeVectorStoreClient();
+    const vectorStoreClient = await getVectorStoreClient();
     vectorStore = await makeVectorStore(vectorStoreClient);
   }
 
   return vectorStore;
+};
+
+/**
+ * Embedding document to existing index
+ */
+export const trainVectorStore = async (
+  documents: Document<DocumentSchema>[],
+): Promise<VectorStore> => {
+  const vectorStoreClient = await getVectorStoreClient();
+
+  const embeddings = new OpenAIEmbeddings({ openAIApiKey: OPEN_AI_API_KEY });
+  const index = vectorStoreClient.Index(PINECONE_INDEX_NAME);
+
+  return await PineconeStore.fromDocuments(documents, embeddings, {
+    pineconeIndex: index,
+    namespace: PINECONE_NAME_SPACE,
+    textKey: PINECONE_TEXT_KEY,
+  });
+};
+
+/**
+ * Delete all vectors in the namespace
+ */
+export const resetVectorStore = async (): Promise<{}> => {
+  const vectorStoreClient = await getVectorStoreClient();
+
+  const index = vectorStoreClient.Index(PINECONE_INDEX_NAME);
+
+  return await index._delete({
+    deleteRequest: {
+      deleteAll: true,
+      namespace: PINECONE_NAME_SPACE,
+    },
+  });
 };
