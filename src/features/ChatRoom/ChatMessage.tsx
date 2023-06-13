a22fa28163da739d206270500fef25f5d6e9b09d
ATK3Shape <Alexandr.Tkachenko@3shape.com>
2023-06-13 13:44:07 +0300

    Align massege blobs

--- src/features/ChatRoom/ChatMessage.tsx
+++ src/features/ChatRoom/ChatMessage.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
 import { HistoryItem, MessageType } from './ChatRoom'
 
-const userTextClass = "mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
-const robotTextClass = "ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
+const userTextClass = "mr-2 py-3 px-4 ml-10 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
+const robotTextClass = "ml-2 py-3 px-4 mr-10 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
 
 const MessageImage = ({ type }: { type: MessageType }): any => {
     const imgSrc = type === 'user' ? '/icon-avatar.svg' : '/icon-robot.svg'
